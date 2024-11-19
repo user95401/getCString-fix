5571593821f4c7ff9d7637f76d9ff43eaa3a9836
user666 <user95401@gmail.com>
2024-11-19 15:31:41 +0400

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@ using namespace geode::prelude;
 class $modify(CCString) {
     const char* getCString() {
         //log::debug("{}(int:{})->{}", this, (int)this, __func__);
+        if ((int)this == 0) log::error("{}(int:{})->{}", this, (int)this, __func__);
         return (int)this != 0 ? CCString::getCString() : CCString::createWithFormat("")->getCString();
     }
 };
+
+$execute{
+    //CCMessageBox(getMod()->getRuntimeInfo().dump().c_str(), (getMod()->getID() + " is loaded").c_str());
+}
